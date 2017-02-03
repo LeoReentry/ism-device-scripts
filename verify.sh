20cf118312ae795322a34cfc3eeae1137cacf568
Leo <leononame@lho.io>
2017-02-03 14:49:39 +0100

    Add script to verify update

---
+++ verify.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Change to home directory
+cd /home/debian
+# Empty fwupdate directory
+rm -rf fwupdate
+mkdir fwupdate
+# Extract fwupdate.tar.gz to fwupdate/
+mv fwupdate.tar.gz fwupdate
+cd fwupdate
+tar -xzf fwupdate.tar.gz
+rm -f fwupdate.tar.gz
+# Calculate checksum and save it as byte file
+sha256sum update.tar | awk '{print $1}' | perl -ne 's/([0-9a-f]{2})/print chr hex $1/gie' > sha256sum
+# If no public key file is present, get a public key file
+if [ ! -f /home/debian/public.pem ]; then
+  keyurl=`python -c "import sys, json; print json.load(sys.stdin)['publicKeyUrl']" < /home/debian/ism-device-server/config.json`
+  wget $keyurl -O /home/debian/public.pem
+  if [ $? -eq 1 ]; then
+    rm public.pem
+    exit 1
+  fi
+fi
+# Verify signature
+openssl pkeyutl -verify -in sha256sum -pubin -inkey /home/debian/public.pem -sigfile sig -pkeyopt digest:sha256
+if [ $? -eq 0 ]; then
+  exit 0
+fi
+exit 1
