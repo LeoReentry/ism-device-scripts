e9c5df46459c1fd21acf90c4f4f28804d31e48eb
Leo <leononame@lho.io>
2017-02-09 11:20:25 +0100

    Fix firmware extraction

--- xfwupdate.sh
+++ xfwupdate.sh
@@ -3,6 +3,7 @@
 # Change to firmware update directory
 cd /home/debian/fwupdate
 # Extract firmware update data
+rm -rf /home/debian/.fwtmp
 mkdir /home/debian/.fwtmp
 tar -xf update.tar -C /home/debian/.fwtmp
 rm -rf /home/debian/fwupdate
