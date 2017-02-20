cc4bd80946011d1a95c93fe708e5df636d500a43
Leo <leononame@lho.io>
2017-02-20 12:02:17 +0100

    Add error indicator

--- files/fwupdate/apply.sh
+++ files/fwupdate/apply.sh
@@ -10,11 +10,31 @@ cd /home/debian
 # Remove all non-hidden files
 echo "------------ Removing old files -----------"
 rm -rf *
+if [ $? -ne 0 ]; then
+  echo "Error"
+  sudo reboot now
+  exit 1
+fi
 cd .fwtmp
+if [ $? -ne 0 ]; then
+  echo "Error"
+  sudo reboot now
+  exit 1
+fi
 echo "----------- Extracting new files ----------"
 tar -xzf home.tgz -C /home/debian/
+if [ $? -ne 0 ]; then
+  echo "Error"
+  sudo reboot now
+  exit 1
+fi
 echo "------ Running user defined settings ------"
 source settings.sh
+if [ $? -ne 0 ]; then
+  echo "Error"
+  sudo reboot now
+  exit 1
+fi
 echo "----------------------------------------------------"
 echo "------ Firmware Update completed successfully ------"
 sudo reboot now
