566283b85ab1b3f07cd6bdc919fbb1dccf4d1b06
Leo <leononame@lho.io>
2017-02-21 09:47:42 +0100

    Update fwupdate scripts to make settings optional

--- files/fwupdate/apply.sh
+++ files/fwupdate/apply.sh
@@ -9,7 +9,7 @@ done;
 
 cd /home/debian
 # Remove all non-hidden files
-echo "------------ Removing old files -----------"
+echo "----------------- Removing old files ----------------"
 rm -rf *
 if [ $? -ne 0 ]; then
   echo "Error"
@@ -22,15 +22,15 @@ if [ $? -ne 0 ]; then
   sudo reboot now
   exit 1
 fi
-echo "----------- Extracting new files ----------"
+echo "---------------- Extracting new files ---------------"
 tar -xzf home.tgz -C /home/debian/
 if [ $? -ne 0 ]; then
   echo "Error"
   sudo reboot now
   exit 1
 fi
-echo "------ Running user defined settings ------"
-source settings.sh
+echo "---------- Running user defined settings -----------"
+[ -f settings.sh ] && source settings.sh || echo "No settings file found"
 if [ $? -ne 0 ]; then
   echo "Error"
   sudo reboot now
